0204d03d53c156cea38ffea8cc04b169c0aec87e
John Akpa Ojonugwa <johnakpaojonugwa@gmail.com>
2026-02-19 13:23:02 +0100

    Edited my CORS logic

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(cors({
     origin: [process.env.CORS_ORIGIN, 'http://localhost:5173', 'http://127.0.0.1:5173'].filter(Boolean),
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
